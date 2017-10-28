58b10599b1fb4d1ba74c824033294240533354cc
brendanwong <mrbrendanwong@gmail.com>
2017-10-28 19:14:37 -0400

    added file i/o

--- main.py
+++ main.py
@@ -1,56 +1,52 @@
 import numpy as np
 import cv2
 
-# import pretrained classifiers
+# import pre-trained haar-cascade classifier
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+# eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+# possible use for eye detection for increased accuracy?
 
 # sets video source to default webcam
 video_capture = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = video_capture.read() # Capture frame by frame
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret, frame = video_capture.read()  # Capture frame by frame
+    if ret is True:
+        # if true, then it was read correctly
+        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert to grayscale
+    else:
+        continue
 
-    # detect face coords and store
-    faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
+    # store and detect face coordinates
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor = 1.7,
+                                          minNeighbors = 5, minSize = (30, 30),
+                                          flags = cv2.CASCADE_SCALE_IMAGE)
 
-    # draw rectangle around the faces
+    # larger scale factor means smaller photo sample and faster face tracking/detection
+    # faces returned as a list of rectangles
+
+    print(faces) # delete later
+
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 2)
+        # draw rectangle around each detected face
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+        # create a crop of just the face
+        cropped = frame[y:y+h, x:x+w]
 
-    # display frame with drawn rectangle
-    cv2.imshow('Video', frame)
+    # frame and cropped are numpy.ndarray's
+    try:
+        # try to display the cropped photo
+        cv2.imshow('cropped', cropped)
+
+        # save the cropped image, will be sent off to microsoft face api
+        cv2.imwrite('face.png', cropped)
+    except:
+        # if error, just show the whole frame
+        cv2.imshow('frame', frame)
 
     # search for face captured?
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    video_capture.release()
-    cv2.destroyAllWindows()
-'''
-img = cv2.imread('dudes.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-
-
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-cv2.imshow('img',img)
-cv2.waitKey(0)
+video_capture.release()
 cv2.destroyAllWindows()
-
-'''
