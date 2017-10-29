a38f3c6807c8dc6564180277888c163f6b40c909
brendanwong <mrbrendanwong@gmail.com>
2017-10-29 03:15:47 -0400

    added tracking labels

--- main-python3.py
+++ main-python3.py
@@ -1,7 +1,50 @@
 import numpy as numpy
 import cv2
 
+class person:
+    def __init__(self, name = None, image = None, relation = None, info = None, met = None):
+        self.name = name
+        self.image = image
+        self.relation = relation
+        self.info = info
+        self.met = met
 
+def rectangle_width(person):
+    str1 = "Name: " + person.name
+    str2 = "Relationship: " + person.relation
+    str3 = "Info: " + person.info
+    str4 = "Met: " + person.met
+    lengths = [len(str1),len(str2),len(str3),len(str4)]
+    maxlen = max(lengths)
+    return maxlen*250/18
+
+def overlay(person,frame,x,y):
+    if x <= 100:
+        cv2.putText(frame, "Name: {}".format(person.name),
+                    (x + 200, y + 90), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 0), 1)
+        cv2.putText(frame, "Relationship: {}".format(person.relation),
+                    (x + 200, y + 110), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 0), 1)
+        cv2.putText(frame, "Info: {}".format(person.info),
+                    (x + 200, y + 130), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 0), 1)
+        cv2.putText(frame, "Met: {}".format(person.met),
+                    (x + 200, y + 150), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 0), 1)
+        rect_width = rectangle_width(person)
+        cv2.rectangle(frame, (x + 200, y + 70), (x + 200 + rect_width, y + 165), 1)
+    else:
+        cv2.putText(frame, "Name: {}".format(person.name),
+                    (x - 300, y + 90), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 0), 1)
+        cv2.putText(frame, "Relationship: {}".format(person.relation),
+                    (x - 300, y + 110), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 0), 1)
+        cv2.putText(frame, "Info: {}".format(person.info),
+                    (x - 300, y + 130), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 0), 1)
+        cv2.putText(frame, "Met: {}".format(person.met),
+                    (x - 300, y + 150), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 0, 0), 1)
+        rect_width = rectangle_width(person)
+        cv2.rectangle(frame, (x - 300, y + 70), (x - 300 + rect_width, y + 165), 1)
+
+person1 = person("Aislinn", cv2.imread("people.jpg"), "friend", "Likes cats", "At school")
+person2 = person("Emma",cv2.imread("people.jpg"), "friend", "cool", "hackathon")
+people = [person1, person2]
 
 def main():
     # import pre-trained haar-cascade classifier
@@ -22,31 +65,42 @@ def main():
 
         # store and detect face coordinates
         faces = face_cascade.detectMultiScale(gray_image, scaleFactor = 1.7,
-                                          minNeighbors = 5, minSize = (30, 30),
+                                          minNeighbors = 5, minSize = (20, 20),
                                           flags = cv2.CASCADE_SCALE_IMAGE)
-
-        # larger scale factor means smaller photo sample and faster face tracking/detection
+        # larger scale factor = smaller photo sample and faster face tracking
         # faces returned as a list of rectangles
 
+        # create a list of faces
+        face_list = []
         for (x, y, w, h) in faces:
-            # draw rectangle around each detected face
+            # draw rectangle around each detected face, can remove later
             cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
             # create a crop of just the face
             cropped = frame[y:y+h, x:x+w]
+            face_plus_coord = [cropped, [x, y, w, h]]
+            face_list.append(face_plus_coord)
+            try:
+                overlay(people[len(face_list)-1], frame, x, y)
+            except:
+                pass
+
+
 
         try:
             # try to display the cropped photo
-            cv2.imshow('cropped', cropped)
+            cv2.imshow('cropped', frame)
+            i = 0
+            for face in face_list:
+                name = "face" + str(i)
+                cv2.imwrite(name, face)
 
-           # save
-            cv2.imwrite('face.png', cropped)
+
             # from here, call functions to send to face api and parse incoming data
         except:
             # if error, just show the whole frame
             cv2.imshow('frame', frame)
 
-
-        # search for face captured?
+        # search for face captured
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
